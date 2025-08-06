b90060e4db525a20af6c4855245af560aec08ba0
Moreshwar Bhonde <ppro67251@gmail.com>
2025-08-06 23:00:12 +0530

    Update config.py

--- security_tester/config.py
+++ security_tester/config.py
@@ -1,1 +1,15 @@
-
+class Config:
+    # Telegram
+    TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+    TELEGRAM_CHAT_ID = "YOUR_TELEGRAM_CHAT_ID"
+    
+    # Testing
+    TEST_OTP = "000000"
+    ATTACK_INTERVAL = 30
+    
+    # Paths
+    CHROMEDRIVER_PATH = "/data/data/com.termux/files/home/chromedriver"
+    
+    # Flask
+    TESTING_PORT = 7700
+    CONTROL_PORT = 7800
