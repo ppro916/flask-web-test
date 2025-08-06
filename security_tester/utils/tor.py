856c3f623ab1add19939d0710f2040ab6a9ca881
Moreshwar Bhonde <ppro67251@gmail.com>
2025-08-06 23:01:07 +0530

    Update tor.py

--- security_tester/utils/tor.py
+++ security_tester/utils/tor.py
@@ -1,1 +1,21 @@
+import requests
+from stem import Signal
+from stem.control import Controller
+from config import Config
 
+def rotate_tor_ip():
+    try:
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate(Config.TOR_PASSWORD)
+            controller.signal(Signal.NEWNYM)
+        return True
+    except Exception as e:
+        return False
+
+def get_tor_session():
+    session = requests.session()
+    session.proxies = {
+        'http': 'socks5://127.0.0.1:9050',
+        'https': 'socks5://127.0.0.1:9050'
+    }
+    return session
